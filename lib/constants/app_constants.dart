1b82d9955e215d9da899102dab4e082d2fb0ae44
Sudipa <sudipasarkar999@gmail.com>
2021-03-16 17:56:33 +0530

    BMI APP

---
+++ lib/constants/app_constants.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+import 'package:hexcolor/hexcolor.dart';
+
+final Color mainHexColor = HexColor("#FCC91C");
+final Color accentHexColor = HexColor("#FF0000");
